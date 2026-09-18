2f7a3a04935bb5b7269bbf4e2168fd154d3f737a
Meghana <meghana.m30062006@gmail.com>
2026-09-18 15:37:36 +0530

    Update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -29,13 +29,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import EventSection from "./components/EventSection";
+import Footer from "./components/Footer";
 function App(){
   return(
     <div>
       <Navbar />
       <main id="home">
       <Hero />
+      <EventSection />
       </main>
+      <Footer />
     </div>
   );
 }
