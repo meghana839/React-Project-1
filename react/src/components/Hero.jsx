a5c395934a7c1c4e120616c14cbc96ed2e73b082
Meghana <meghana.m30062006@gmail.com>
2026-09-18 12:36:42 +0530

    updated

---
+++ react/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero(){
+    return(
+        <section className="hero">
+            <p className="hero-label">College Events and Clubs</p>
+            <h1>Discover what is happening?</h1>
+            <p className="hero-description">Find Workshops,Sports,Activities,Club metting</p>
+            <a className="hero-button" href="#events">Explore Events</a>
+        </section>
+
+    );
+}
+export default Hero;
