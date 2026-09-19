f60458bb33bfea0d5333cf2bdda4f271b60bdd43
Meghana <meghana.m30062006@gmail.com>
2026-09-19 16:11:32 +0530

    Updatedd

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,49 @@
+// import { useState } from "react";
+// import "./App.css";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import EventForm from "./components/EventForm";
+// import EventSection from "./components/EventSection";
+// import Footer from "./components/Footer";
+// import { initialEvents } from "./data/events";
+
+// function App() {
+//   const [events, setEvents] = useState(initialEvents);
+
+//   function handleAddEvent(newEvent) {
+//     setEvents([...events, newEvent]);
+//   }
+
+//   return (
+//     <div>
+//       <Navbar />
+
+//       <main id="home">
+//         <Hero
+//           title="Discover What Is Happening on Campus"
+//           description="Find workshops, sports activities, club meetings, and opportunities to connect with other students."
+//         />
+
+//         <EventForm onAddEvent={handleAddEvent} />
+
+//         <EventSection events={events} />
+//       </main>
+
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
+import { Routes, Route } from "react-router";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import EventForm from "./components/EventForm";
-import EventSection from "./components/EventSection";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import AboutPage from "./pages/AboutPage";
 import { initialEvents } from "./data/events";
 
 function App() {
@@ -18,16 +57,18 @@ function App() {
     <div>
       <Navbar />
 
-      <main id="home">
-        <Hero
-          title="Discover What Is Happening on Campus"
-          description="Find workshops, sports activities, club meetings, and opportunities to connect with other students."
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage events={events} onAddEvent={handleAddEvent} />
+          }
         />
 
-        <EventForm onAddEvent={handleAddEvent} />
+        <Route path="/events" element={<EventsPage events={events} />} />
 
-        <EventSection events={events} />
-      </main>
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
 
       <Footer />
     </div>
